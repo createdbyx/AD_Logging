59cec9935db08aff702d797786d6266ecc33280c
Dean Lunz <deanlunz@createdbyx.com>
2020-02-26 11:08:52 -0800

    initial commit

---
+++ ADLogger.cs
@@ -0,0 +1,45 @@
+﻿using Codefarts.AutoDownloader;
+
+namespace Codefarts.AutoDownloader.Plugins
+{
+    using System;
+    using Codefarts.Logging;
+
+    public class ADLogger : ILogging
+    {
+        private ApplicationModel appModel;
+
+        public ADLogger(ApplicationModel appModel)
+        {
+            this.appModel = appModel;
+        }
+
+        public void Log(string message)
+        {
+            this.Log(LogEntryType.Generic, message, null);
+        }
+
+        public void Log(LogEntryType type, string message)
+        {
+            this.Log(type, message, null);
+        }
+
+        public void Log(string message, string category)
+        {
+            this.Log(LogEntryType.Generic, message, category);
+        }
+
+        public void Log(LogEntryType type, string message, string category)
+        {
+            var data = DateTime.Now.ToString("u");
+            data += " " + type;
+            data += " " + (string.IsNullOrWhiteSpace(category) ? string.Empty : "(" + category + ")");
+            data += " " + message;
+
+            //var ioc = Container.Default;
+            //var platform = ioc.Resolve<IPlatformProvider>();
+            // platform.OnUIThread(() => this.appModel.Logging.Logs.Add(data));
+            this.appModel.Logging.Logs.Add(data);
+        }
+    }
+}
---
+++ ADPlugin.cs
@@ -0,0 +1,47 @@
+﻿using Codefarts.AutoDownloader;
+using Codefarts.AutoDownloader.Interfaces;
+using Codefarts.AutoDownloader.Plugins;
+
+namespace Codefarts.AutoDownloader.Plugins
+{                                              
+    using Codefarts.Logging;
+    using System.ComponentModel.Composition;
+
+    [Export(typeof(IGeneralPlugin))]
+    [GeneralPlugin("Auto Downloader Logging")]
+    public class ADPlugin : IGeneralPlugin
+    {
+        private ADLogger logger;
+        private ApplicationModel applicationModel;
+
+        public string Title
+        {
+            get
+            {
+                return "Auto Downloader Logging";
+            }
+        }
+
+      
+        public void Connect(ApplicationModel appModel)
+        {
+            this.applicationModel = appModel;
+            this.logger = new ADLogger(appModel);
+            // Logging.LoggedEntry += this.Logging_LoggedEntry;
+            Logs.Instance.Repositories.Add(this.logger);
+        }
+
+        //private void Logging_LoggedEntry(object sender, LogModelEventArgs e)
+        //{
+        //    this.applicationModel.Logging.Logs.Add(e.Message);
+        //}
+
+        public void Disconnect()
+        {
+            // Logging.LoggedEntry -= this.Logging_LoggedEntry;
+            Logs.Instance.Repositories.Remove(this.logger);
+            this.logger = null;
+            this.applicationModel = null;
+        }
+    }
+}
